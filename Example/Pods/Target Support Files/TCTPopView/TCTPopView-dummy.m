00cb39b3f064dbc419822f89a536c06f47badff9
le tong <le.tong@ly.com>
2019-11-19 17:38:59 +0800

    first commit

---
+++ Example/Pods/Target Support Files/TCTPopView/TCTPopView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TCTPopView : NSObject
+@end
+@implementation PodsDummy_TCTPopView
+@end
---
+++ TCTPopView/Classes/PopView.h
@@ -0,0 +1,15 @@
+//
+//  PopView.h
+//  ShowViewController
+//
+//  Created by iOS on 2018/7/24.
+//  Copyright © 2018年 iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+typedef void (^myBlock)(BOOL isLeft);
+@interface PopView : UIView
+@property (nonatomic , copy)myBlock block;
++ (instancetype)popViewblocks:(myBlock)blocks;
+
+@end
